0f5cabcba1da1e56653ad7cdc8b0e245748520da
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2021-12-25 00:02:52 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import Style1 from "./components/Style1";
+// import App from "./App";
+// import Style1 from "./components/Style1";
 
-import Counter from "./Counter";
-import store from "./store";
-import { Provider } from "react-redux";
-import ViewOne from "./ViewOne";
-import AsyncArray from "./components/AsyncArray";
+// import Counter from "./Counter";
+// import store from "./store";
+// import { Provider } from "react-redux";
+// import ViewOne from "./ViewOne";
+// import AsyncArray from "./components/AsyncArray";
+import Example from "./animations/Refresh";
 
-ReactDOM.render(
-  // <Provider store={store}>
-  //   <Counter />
-  // </Provider>,
-  // <ViewOne />,
-  <AsyncArray />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Example />, document.getElementById("root"));
