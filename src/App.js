f741fbf093a3b774e7343941d6a1957b0360d68f
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-01-14 17:36:40 +0530

    explain to kanisha

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import { useState } from "react";
 
-function App() {
-  const [id, setId] = useState("");
-  const [name, setName] = useState("");
-  const dropdown = [
-    { id: 1, title: "one" },
-    { id: 2, title: "two" },
-    { id: 3, title: "three" },
-    { id: 4, title: "four" },
-    { id: 5, title: "five" },
-  ];
+const App = () => {
+  // javascript
+  const [names, setNames] = useState([{ name: "saman" }, { name: "kamal" }]);
+  const [customName, setcustomName] = useState("");
 
-  const onGetId = (e) => {
-    const id = e.target.value;
-    setId(id);
-    const findItem = dropdown.find((d) => d.id === +id);
-    setName(findItem.title);
+  const addName = () => {
+    setNames((prev) => [...prev, { name: customName }]);
   };
 
   return (
     <div>
-      <select onChange={onGetId}>
-        {dropdown.map((d) => (
-          <option key={d.id} value={d.id}>
-            {d.title}
-          </option>
-        ))}
-      </select>
-      {id && <p> Id : {id} </p>}
-      {name && <p> Name : {name} </p>}
+      <div className="section" style={{ display: "flex", margin: "20px" }}>
+        <p> Home </p>
+        <p> Contact us </p>
+        <p> About us </p>
+      </div>
+      Hello World
+      <input
+        value={customName}
+        onChange={(e) => setcustomName(e.target.value)}
+      />
+      {names.map((nam, index) => (
+        <p key={index}> {nam.name} </p>
+      ))}
+      <p onClick={addName}> Add Name </p>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./footer/Footer.module.css"
-// import App from "./App";
-// import Style1 from "./components/Style1";
 
-// import Counter from "./Counter";
-// import store from "./store";
-// import { Provider } from "react-redux";
-// import ViewOne from "./ViewOne";
-// import AsyncArray from "./components/AsyncArray";
-import Example from "./animations/Refresh";
-import A from "./Data";
-import Footer from './footer/Footer'
-import OneSignal from './one-signal/OneSignal'
+import Hello from "./App";
 
-
-ReactDOM.render(<OneSignal />, document.getElementById("root"));
+ReactDOM.render(<Hello />, document.getElementById("root"));
