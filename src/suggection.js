b031d9c84b7e8ca276adf0d9a7fd74845cb2236a
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-07-25 21:00:39 +0530

    try for suggestions

---
+++ src/suggection.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const dropdown = [
+    { id: 1, title: "one" },
+    { id: 2, title: "two" },
+    { id: 3, title: "three" },
+    { id: 4, title: "four" },
+    { id: 5, title: "five" },
+  ];
+
+  const onGetId = (e) => {
+    const id = e.target.value;
+    setId(id);
+    const findItem = dropdown.find((d) => d.id === +id);
+    setName(findItem.title);
+  };
+
+  return (
+    <div>
+      <select onChange={onGetId}>
+        {dropdown.map((d) => (
+          <option key={d.id} value={d.id}>
+            {d.title}
+          </option>
+        ))}
+      </select>
+      {id && <p> Id : {id} </p>}
+      {name && <p> Name : {name} </p>}
+    </div>
+  );
+}
+
+export default App;
